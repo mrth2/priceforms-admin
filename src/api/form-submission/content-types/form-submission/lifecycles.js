d73728a988400084da3abfd91c4e04d20a110075
mrth2 <mr.th2.90@gmail.com>
2022-03-18 16:42:14 +0700

    disable form submission lifecycle to auto send email to form owner

--- src/api/form-submission/content-types/form-submission/lifecycles.js
+++ src/api/form-submission/content-types/form-submission/lifecycles.js
@@ -17,36 +17,37 @@ function getFullName(user) {
 module.exports = {
   // auth send email to form owner whenever a new submission is created
   async afterCreate(event) {
-    const { result, params } = event;
-    const email = {
-      to: result.owner.email,
-      template_id: 'd-666eb7c3184940459756f38e981d134f',
-      dynamic_template_data: {
-        client: {
-          name: getFullName(result.owner)
-        },
-        form: {
-          name: result.form.subDomain.charAt(0) + result.form.subDomain.substring(1).toLowerCase(),
-          case: result.category.title
-        },
-        user: {
-          name: getFullName(result.user),
-          email: result.user.email
-        },
-        submissionURL: `https://${result.form.subDomain}.${process.env.FRONTEND_DOMAIN}/admin/submissions/${result.id}`
-      }
-    };
-    try {
-      await strapi
-        .plugin('email')
-        .service('email')
-        .send(email);
-    } catch (e) {
-      if (e.statusCode === 400) {
-        throw new ApplicationError(e.message);
-      } else {
-        throw new Error(`Couldn't send test email: ${e.message}.`);
-      }
-    }
+    // const { result, params } = event;
+    // console.log(result);
+    // const email = {
+    //   to: result.owner.email,
+    //   template_id: 'd-666eb7c3184940459756f38e981d134f',
+    //   dynamic_template_data: {
+    //     client: {
+    //       name: getFullName(result.owner)
+    //     },
+    //     form: {
+    //       name: result.form.subDomain.charAt(0) + result.form.subDomain.substring(1).toLowerCase(),
+    //       case: result.category.title
+    //     },
+    //     user: {
+    //       name: getFullName(result.subscriber),
+    //       email: result.subscriber.email
+    //     },
+    //     submissionURL: `https://${result.form.subDomain}.${process.env.FRONTEND_DOMAIN}/admin/submissions/${result.id}`
+    //   }
+    // };
+    // try {
+    //   await strapi
+    //     .plugin('email')
+    //     .service('email')
+    //     .send(email);
+    // } catch (e) {
+    //   if (e.statusCode === 400) {
+    //     throw new ApplicationError(e.message);
+    //   } else {
+    //     throw new Error(`Couldn't send test email: ${e.message}.`);
+    //   }
+    // }
   }
 };
