c10543d363358f5cbf514ac902194bb1470a008b
mrth2 <mr.th2.90@gmail.com>
2022-03-30 00:27:27 +0700

    request for magic link and auto open to login

--- src/plugins/magic-link/admin/src/components/PluginIcon/index.js
+++ src/plugins/magic-link/admin/src/components/PluginIcon/index.js
@@ -5,8 +5,8 @@
  */
 
 import React from 'react';
-import Puzzle from '@strapi/icons/Puzzle';
+import Link from '@strapi/icons/Link';
 
-const PluginIcon = () => <Puzzle />;
+const PluginIcon = () => <Link />;
 
 export default PluginIcon;
