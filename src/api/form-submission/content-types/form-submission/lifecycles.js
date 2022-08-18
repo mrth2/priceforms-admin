74b70b869d8e0cdd605e736053451123044f9c7f
mrth2 <mr.th2.90@gmail.com>
2022-08-18 09:42:07 +0700

    disable lifecycle to notify submission complete after update submission

--- src/api/form-submission/content-types/form-submission/lifecycles.js
+++ src/api/form-submission/content-types/form-submission/lifecycles.js
@@ -1,14 +1,14 @@
 module.exports = {
   // auth send email to form owner whenever a new submission is updated with completed status
-  async afterUpdate(event) {
-    const { result } = event;
-    // get submission
-    const submission = await strapi.db.query('api::form-submission.form-submission').findOne({
-      where: {
-        id: result.id
-      },
-      populate: ['form', 'category', 'subscriber', 'owner']
-    });
-    await strapi.service('api::form-submission.form-submission').notifyNewSubmission(submission);
-  }
+  // async afterUpdate(event) {
+  //   const { result } = event;
+  //   // get submission
+  //   const submission = await strapi.db.query('api::form-submission.form-submission').findOne({
+  //     where: {
+  //       id: result.id
+  //     },
+  //     populate: ['form', 'category', 'subscriber', 'owner']
+  //   });
+  //   await strapi.service('api::form-submission.form-submission').notifyNewSubmission(submission);
+  // }
 };
