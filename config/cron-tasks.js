5c52439ffdb63226a2c804bedc534a4df8dc4e11
mrth2 <mr.th2.90@gmail.com>
2022-08-21 13:47:40 +0700

    cronjob only query completed submission to notify

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -1,10 +1,11 @@
 module.exports = {
   // cronjob run once per 10 minutes to notify un-notified submission
   '*/10 * * * *': async ({ strapi }) => {
-    // query all non-notified submissions are older than 5 minutes of inactivity
+    // query all completed but non-notified submissions are older than 5 minutes of inactivity
     // and mark them as notified
     const submissions = await strapi.db.query('api::form-submission.form-submission').findMany({
       where: {
+        status: 'complete',
         $or: [
           {
             notified: false,
