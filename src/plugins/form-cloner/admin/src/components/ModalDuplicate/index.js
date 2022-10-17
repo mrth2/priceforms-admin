383c6c7139940b2ac1f6e335935d6f584e26e498
mrth2 <mr.th2.90@gmail.com>
2022-10-17 10:15:45 +0700

    update duplicate modal and auto redirect to new form after clone successfully

--- src/plugins/form-cloner/admin/src/components/ModalDuplicate/index.js
+++ src/plugins/form-cloner/admin/src/components/ModalDuplicate/index.js
@@ -6,23 +6,38 @@ import { Loader } from '@strapi/design-system/Loader';
 import { TextInput } from '@strapi/design-system/TextInput';
 import { Button } from '@strapi/design-system/Button';
 import { Alert } from '@strapi/design-system/Alert';
+import axiosInstance from '../../utils/axiosInstance';
 
 function ModalDuplicate({ form, closeModal }) {
-  console.log(form);
   const [isLoading, setIsLoading] = React.useState(false);
   const [subDomain, setSubDomain] = React.useState('');
   const [error, setError] = React.useState(null);
+  const [success, setSuccess] = React.useState(null);
 
-  const cloneForm = React.useCallback(() => {
+  const cloneForm = React.useCallback(async () => {
     if (!subDomain) {
       setError('Please enter a subdomain');
       return;
     }
     setError(null);
     setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
+    try {
+      const { data: { newForm } } = await axiosInstance.post('/form-cloner/start', {
+        data: {
+          ...form,
+          subDomain: subDomain.toLowerCase(),
+        }
+      });
+      if (newForm) {
+        setSuccess('Form cloned successfully to ' + newForm.subDomain);
+        setSubDomain('');
+        window.location.href = `/admin/content-manager/collectionType/api::form.form/${newForm.id}?plugins[i18n][locale]=en`
+      }
+    } catch (e) {
+      console.log(e);
+      setError(e.response?.data?.error?.message || e.message);
+    }
+    setIsLoading(false);
   }, [isLoading, subDomain]);
   return (
     <ModalLayout onClose={closeModal}>
@@ -40,15 +55,18 @@ function ModalDuplicate({ form, closeModal }) {
         ) : (
           <>
             {error && (
-              <Alert closeLabel="Close alert" title={error} variant="danger" style={{ marginBottom: 20 }}>{error}</Alert>
+              <Alert closeLabel="Close alert" variant="danger" style={{ marginBottom: 20 }} onClose={() => setError(null)}>{error}</Alert>
             )}
-            < TextInput
+            {success && (
+              <Alert closeLabel="Close alert" variant="success" style={{ marginBottom: 20 }} onClose={() => setSuccess(null)}>{success}</Alert>
+            )}
+            <TextInput
               placeholder="Enter new subdomain"
               label="Subdomain"
               name="subdomain"
               hint={`Eg: "newform" to have it running via newform.priceforms.net`}
               value={subDomain}
-              onChange={e => setSubDomain(e.target.value)}
+              onChange={e => setSubDomain(e.target.value.toLowerCase())}
             />
           </>
         )}
